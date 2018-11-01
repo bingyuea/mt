69fa46d874a85308d3736f12f664c3220eb01262
songfangyuan <songfangyuan@zto.cn>
2018-11-01 18:22:11 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from 'react'
+import 'antd-mobile/dist/antd-mobile.css'
+// import 'antd-mobile/dist/antd-mobile.less' // or ''
+import './App.css'
+import { Button } from 'antd-mobile'
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <header className="App-header" />
+        <div>
+          <Button>Start</Button>
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
