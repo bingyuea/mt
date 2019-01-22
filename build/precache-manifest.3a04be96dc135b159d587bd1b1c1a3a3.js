d2f6d4d345d3d9edbdc6d100e218cb93c73f1e3b
songfangyuan <songfangyuan@zto.cn>
2019-01-22 21:08:43 +0800

    添加图片

---
+++ build/precache-manifest.3a04be96dc135b159d587bd1b1c1a3a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4509a1d0e1277828b3a94be76ae70d97",
+    "url": "./static/media/gold.4509a1d0.jpg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "216a1fa3257315792aab",
+    "url": "./static/js/main.216a1fa3.chunk.js"
+  },
+  {
+    "revision": "597fe54b1cff40eff4f6",
+    "url": "./static/js/1.597fe54b.chunk.js"
+  },
+  {
+    "revision": "216a1fa3257315792aab",
+    "url": "./static/css/main.e49f6f41.chunk.css"
+  },
+  {
+    "revision": "597fe54b1cff40eff4f6",
+    "url": "./static/css/1.7d20038e.chunk.css"
+  },
+  {
+    "revision": "7557373a8c06047f9221381a3c20e839",
+    "url": "./index.html"
+  }
+];
